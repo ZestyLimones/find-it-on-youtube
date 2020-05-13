3c977dc99debab340d4c57612d436ebb1b8c8d06
Micheal Limones <michlimones@gmail.com>
2020-05-13 11:45:55 -0600

    added code to html, js, and css

---
+++ index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const apiKey = 'AIzaSyDeB92PTcUaALWe2F0szjrdcqeoulB7E4M'; 
+const searchURL = 'https://www.googleapis.com/youtube/v3/search';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  $('#results-list').empty();
+  for (let i = 0; i < responseJson.items.length; i++){
+    $('#results-list').append(
+      `<li><h3>${responseJson.items[i].snippet.title}</h3>
+      <p>${responseJson.items[i].snippet.description}</p>
+      <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
+      </li>`
+    )};
+  $('#results').removeClass('hidden');
+};
+
+function getYouTubeVideos(query, maxResults=10) {
+  const params = {
+    key: apiKey,
+    q: query,
+    part: 'snippet',
+    maxResults,
+    type: 'video'
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+
+  console.log(url);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    const maxResults = $('#js-max-results').val();
+    getYouTubeVideos(searchTerm, maxResults);
+  });
+}
+
+$(watchForm);
